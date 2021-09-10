71d5212c6a2b61c56b46347ddb479f564c68bf9b
Castellanos <alexcastellanos442@gmail.com>
2021-09-09 20:21:44 -0500

    Added more features

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-const rate=document.querySelector("#rate")
-const amount = document.querySelector("#amount")
-const rateValue= document.querySelector("#rateNumber")
+const rate = document.querySelector("#rate")
+const amount = document.querySelector("#principal")
+const rateValue = document.querySelector("#rateNumber")
 const years = document.querySelector("#years")
-const btn = document.getElementById("btn")
-const end = document.getElementById("end")
-const btnEvent= btn.addEventListener("click",()=>{
-    const amountValue =Number(amount.value)
+const btn = document.querySelector("#calculate")
+const square = document.querySelector(".square")
+
+const footer = document.querySelector('footer')
+
+const btnEvent = btn.addEventListener("click", () => {
+    const amountValue = Number(amount.value)
+    if (amountValue <= 0) {
+        alert("You need to provide a positive number here!")
+        amount.focus()
+        return
+    }
     const rateValue = Number(rate.value)
-    const numYears= Number(years.value)
-const interest= amountValue*rateValue/100 *numYears
+    const numYears = Number(years.value)
+    const interest = amountValue * rateValue / 100 * numYears
     console.log(interest)
-    end.insertAdjacentHTML('afterend',`<p>If you deposit ${amountValue}$, <br> at an interest rate of ${rateValue}% <br> You will receive an amount of  ${interest} <br> in the year ${2021+numYears}</p>`)
+
+    const newDiv = document.createElement('div')
+    const htmlContent = `<div> If you deposit  <mark>${amountValue} </mark>$, <br> at an interest rate of <mark>${rateValue}% </mark>,<br> you will receive an amount of  <mark>${interest}</mark>, <br> in the year <mark>${2021 + numYears}</mark>. </div>`
+    footer.insertAdjacentHTML('beforebegin', htmlContent)
+
+
+
+
+
 });
 
-const rangeValue= rate.addEventListener("change",()=>{rateValue.value= rate.value})
+const rangeValue = rate.addEventListener("change", () => { rateValue.value = `${rate.value}%` })
 
 
-        
